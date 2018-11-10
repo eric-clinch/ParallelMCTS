9ae29d5a3458194e2815fdee596f3f21e6d43c76
Jennifer Lee <jennife4@andrew.cmu.edu>
2018-11-10 15:49:04 -0500

    pushing starter board code

--- code/Board.cpp
+++ code/Board.cpp
@@ -4,18 +4,55 @@ using namespace std;
 
 class Board {
 
+    private:
+    int width, height;
+    char **board;
+
+    public:
+    Board() {
+        int width = 19;
+        int height = 19;
+        char **Board = malloc(height * sizeof(*char));
+        for (int i = 0; i < height; i++) {
+            Board[i] = malloc(width * sizeof(char));
+        }
+        return Board;
+    }
+
+    Board(int w, int h) {
+        int width = w;
+        int height = h;
+        char **Board = malloc(height * sizeof(char*));
+        for (int i = 0; i < height; i++) {
+            Board[i] = malloc(width * sizeof(char));
+        }
+        return Board;
+
+    }
+
+    virtual Board *getCopy() {
+        Board blankBoard = malloc(sizeof(char*) * height);
+        for (int i = 0; i < height; i++) {
+            blankBoard[i] = malloc(width * sizeof(char));
+            memcpy(blankBoard[i], board[i], width * sizeof(char));
+        }
+        return blankBoard;
+    }
+
+    virtual inline int getWidth() {
+        return width;
+    }
+
+    virtual inline int getHeight() {
+        return height;
+    }
+
+
+
     static inline void deleteBoard() {
         for (int i = 0; i < height; i++) {
             free(board[i]);
         }
         free(board);
     }
-
-    virtual inline void copyBoard(Board &blankBoard) {
-        for (int i = 0; i < height; i++) {
-            memcpy(blankBoard[i], board[i], width * sizeof(char));
-        }
-    }
-
-
 }
