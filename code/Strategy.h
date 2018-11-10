8a5e8a22620ff7dd1499037621015e939234454b
Jennifer Lee <jennife4@andrew.cmu.edu>
2018-11-10 16:19:03 -0500

    Eric fix .cpp code por favor

--- code/Strategy.h
+++ code/Strategy.h
@@ -6,8 +6,10 @@
 
 class Strategy {
 public:
-	virtual Move getMove(Board &board, Player playerID, Player enemyID, int totalTime, int timePerMove, int round) = 0;
-	virtual void cleanUp() {} // needs to be called at the end of each game to allow freeing of memory
+	virtual Move getMove(Board &board, Player playerID, Player enemyID, 
+            int totalTime, int timePerMove, int round) = 0;
+	virtual void cleanUp() {} // needs to be called at the end of each game to 
+                            // allow freeing of memory
 	virtual string toString() { return "Strategy"; }
 };
 
