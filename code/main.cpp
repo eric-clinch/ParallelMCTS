2a99fb973ac78a1214cea5c95361006211749d47
eric-clinch <eric.c.clinch@gmail.com>
2018-11-24 00:28:21 -0500

    implemented playout parallelism with worker threads

--- code/main.cpp
+++ code/main.cpp
@@ -1,11 +1,39 @@
 
 #include <assert.h>
+#include <atomic>
+#include <cstring>
 #include <iostream>
 #include "Board.h"
 #include "Game.h"
 #include "MCTS.h"
 #include "Move.h"
 #include "Strategy.h"
+
+// option parsing code taken from 418 course starter code
+static int _argc;
+static const char** _argv;
+
+const char* get_option_string(const char* option_name,
+                              const char* default_value) {
+  for (int i = _argc - 2; i >= 0; i -= 2) {
+    if (strcmp(_argv[i], option_name) == 0) return _argv[i + 1];
+  }
+  return default_value;
+}
+
+int get_option_int(const char* option_name, int default_value) {
+  for (int i = _argc - 2; i >= 0; i -= 2) {
+    if (strcmp(_argv[i], option_name) == 0) return atoi(_argv[i + 1]);
+  }
+  return default_value;
+}
+
+float get_option_float(const char* option_name, float default_value) {
+  for (int i = _argc - 2; i >= 0; i -= 2) {
+    if (strcmp(_argv[i], option_name) == 0) return (float)atof(_argv[i + 1]);
+  }
+  return default_value;
+}
 
 void zeroFill(bool* seen, int length) {
   for (int i = 0; i < length; i++) seen[i] = false;
@@ -144,7 +172,13 @@ Board test7() {
   return board;
 }
 
-int main() {
+int main(int argc, const char* argv[]) {
+  _argc = argc - 1;
+  _argv = argv + 1;
+
+  int secondsPerMove = 1000 * get_option_int("-t", 1);
+  int threads = get_option_int("-p", 1);
+
   bool* seen = new bool[19 * 19];
   zeroFill(seen, 19 * 19);
 
@@ -206,8 +240,8 @@ int main() {
 
   delete[] seen;
 
-  Strategy* S0 = new MCTS(20000, 2);
-  Strategy* S1 = new MCTS(20000, 2);
+  Strategy* S0 = new MCTS(secondsPerMove, threads);
+  Strategy* S1 = new MCTS(secondsPerMove, threads);
 
   Game* G = new Game(S0, S1, 1);
   int s0_wins = G->runGame();
